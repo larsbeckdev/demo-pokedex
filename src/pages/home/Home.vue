d41dee8f601a3f821075ff1665e3d3a7387685d5
larsbeckmusic <larsbeckmusic@gmail.com>
2026-02-06 08:57:44 +0000

    Add Pokedex component to the Home page template

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,11 +1,9 @@
 <script setup>
-import
+import Pokedex from "@/features/pokedex/Pokedex.vue";
 </script>
 
 <template>
-    
+  <Pokedex />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
